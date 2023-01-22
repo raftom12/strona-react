9b67935952b887ccc6e246cb047aed9e640faacd
raftom12 <120105154+raftom12@users.noreply.github.com>
2023-01-23 00:15:04 +0100

    Update AdminPanel.tsx

--- src/components/admin/AdminPanel.tsx
+++ src/components/admin/AdminPanel.tsx
@@ -12,39 +12,97 @@ export default function AdminPanel() {
     const [char1data, setChar1data] = useState(0);
     const [label1, setLabel1] = useState([]);
     const [value1, setValue1] = useState([]);
-    ChartJS.register(ArcElement, Tooltip, Legend);
+    const [value2, setValue2] = useState([]);
+    const [value3, setValue3] = useState([]);
 
-    const loadPostList = (page: number) => {
+
+    const loadPostList = () => {
         setTimeout(() => {
-            axios.get(`https://localhost:7106/api/posts/${page}`,
+            axios.get(`https://localhost:7106/api/admin/UserActivityStatsAllTime`,
                       {
                           headers: { Authorization: `Bearer ${localStorage.getItem('_auth')}`}}
                           )
-            .then((res) => {
+            .then((res:any) => {
                 console.log(res);
-                for(const dataObj of res.data){
-                    label1.push(dataObj.labels);
-                    value1.push(parseInt(dataObj.value ));
-                }})
+                //@ts-ignore
+                const newList = label1.concat(res.data);
+                console.log(newList);
+                //@ts-ignore
+                const labels = newList[0].userName;
+                //@ts-ignore
+                let label = newList.map(({ userName }) => userName);
+                let value1 = newList.map(({ postsCount }) => postsCount);
+                let value2 = newList.map(({ commentsCount }) => commentsCount);
+                let value3 = newList.map(({ filesCount }) => filesCount);
+                setLabel1(label);
+                setValue1(value1);
+                setValue1(value2);
+                setValue1(value3);
+                })
             .catch((err) => {
                 console.log(err);
             })
 
         }, 15)
     }
+    useEffect(() => {
+        loadPostList();
+        }, [])
     const data1 = {
-        labels: [],
+        labels: label1,
         datasets: [
             {
                 label: '# of Votes',
-                data: [1, 20000000],
+                data: value1,
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)'
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 62, 135, 0.2)'
                     ],
                 borderColor: [
                     'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)'
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 62, 135, 1)'
+                ],
+                borderWidth: 1,
+            },
+            ],
+    };
+    const data2 = {
+        labels: label1,
+        datasets: [
+            {
+                label: '# of Votes',
+                data: value2,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 62, 135, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 62, 135, 1)'
+                ],
+                borderWidth: 1,
+            },
+            ],
+    };
+    const data3 = {
+        labels: label1,
+        datasets: [
+            {
+                label: '# of Votes',
+                data: value3,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 62, 135, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 62, 135, 1)'
                 ],
                 borderWidth: 1,
             },
@@ -61,21 +119,40 @@ export default function AdminPanel() {
     return (
             <Container className={'cont'}>
             <Row xs={1} md={2} className="g-4">
-                {Array.from({ length: 4 }).map((_, idx) => (
                         <Col>
                             <Card>
                                 <Pie data={data1} />
                                 <Card.Body>
-                                    <Card.Title>Card title</Card.Title>
+                                    <Card.Title>Posts </Card.Title>
                                     <Card.Text>
-                                        This is a longer card with supporting text below as a natural
-                                        lead-in to additional content. This content is a little bit
-                                        longer.
+                                        which user wrote the most?
                                     </Card.Text>
                                 </Card.Body>
                             </Card>
                         </Col>
-                        ))}
+                        <Col>
+                            <Card>
+                                <Pie data={data2} />
+                                <Card.Body>
+                                    <Card.Title>Comments </Card.Title>
+                                    <Card.Text>
+                                        which user wrote the most?
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card>
+                                <Pie data={data3} />
+                                <Card.Body>
+                                    <Card.Title>Files </Card.Title>
+                                    <Card.Text>
+                                        which user add the most?
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
             </Row>
             </Container>
             );
